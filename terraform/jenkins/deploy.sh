24ec7d65db11e30c0cc166c2f79fa531927dfe24
James <jimmyhaugh@googlemail.com>
2020-02-06 00:02:16 +0000

    add try/except to deploy.sh

--- terraform/jenkins/deploy.sh
+++ terraform/jenkins/deploy.sh
@@ -6,6 +6,7 @@ SLAVE_IP=$(terraform output slave_ip)
 SSH_PROFILE=$(terraform output project_name)
 KEY_NAME=$(terraform output key_name)
 
+MASTER_PROFILE="jenkins_master"
 SLAVE_PROFILE="jenkins_slave"
 SSH_USER="ubuntu"
 SSH_PORT=22
@@ -13,5 +14,5 @@ SSH_PORT=22
 IDENTITY_FILE="~/.ssh/${KEY_NAME}.pem"
 
 echo "Updating ${SSH_PROFILE} SSH Profile"
-storm edit $SSH_PROFILE --id_file $IDENTITY_FILE "${SSH_USER}@${REMOTE_IP}:${SSH_PORT}"
-storm edit $SLAVE_PROFILE --id_file $IDENTITY_FILE "${SSH_USER}@${SLAVE_IP}:${SSH_PORT}"
+storm add $MASTER_PROFILE --id_file $IDENTITY_FILE "${SSH_USER}@${REMOTE_IP}:${SSH_PORT}" || storm edit $MASTER_PROFILE --id_file $IDENTITY_FILE "${SSH_USER}@${REMOTE_IP}:${SSH_PORT}"
+storm add $SLAVE_PROFILE --id_file $IDENTITY_FILE "${SSH_USER}@${SLAVE_IP}:${SSH_PORT}" || storm edit $SLAVE_PROFILE --id_file $IDENTITY_FILE "${SSH_USER}@${SLAVE_IP}:${SSH_PORT}"
